c5fd0a82fb26abebd5fb9b5f5fe278b9d84d2649
fewudi <130993642+fewudi@users.noreply.github.com>
2023-04-19 09:09:48 -0400

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Circle(){
+  private double r;
+  public static double PI = Math.PI;
+  public Circle(double radius){
+     r = radius; 
+  }
+  
+  public double getRadius(){
+    return r; 
+  }
+  
+  public double getArea(){
+    return r * r * PI;
+  }
+}
